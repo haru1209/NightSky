d9338e7662f644b237ef9ad544cf087c5dde66fc
MSI\cutem <cutemiffy1209@gmail.com>
2026-01-21 23:42:16 +0800

    Finish star generating feature

--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
 const inputElement = document.getElementById("input");
+const body = document.querySelector("body");
+const discriptionContainerElement = document.querySelector(
+  ".discription-container",
+);
 let inputValue;
 
 inputElement.addEventListener("input", (e) => {
   inputValue = e.target.value;
-  console.log("input:", inputValue);
+  // console.log("input:", inputValue);
 });
 
 inputElement.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    console.log("enter:", inputValue);
+    discriptionContainerElement.classList.add("transparent");
+    createStars(inputValue);
+    // console.log("enter:", inputValue);
   }
 });
+
+const createStars = (count) => {
+  console.log(count);
+  for (let i = 0; i < count; i++) {
+    let starElement = document.createElement("span");
+    starElement.className = "star";
+    starElement.textContent = "*";
+    starElement.style.left = Math.random() * window.innerWidth + "px";
+    starElement.style.top = Math.random() * window.innerHeight + "px";
+
+    setTimeout(() => {
+      body.appendChild(starElement);
+    }, 100 * i);
+  }
+};
